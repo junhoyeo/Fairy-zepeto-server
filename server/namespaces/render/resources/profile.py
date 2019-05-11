e727779ba5139ae088db586de0ebfd5425278ae4
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-11 16:19:37 +0900

    이런 식으로 렌더링된 파일 보내주면 됨

--- server/namespaces/render/resources/profile.py
+++ server/namespaces/render/resources/profile.py
@@ -1,3 +1,4 @@
+from flask import send_file
 from flask_restplus import Resource
 from server.namespaces.render import render_ns
 
@@ -5,7 +6,6 @@ from server.namespaces.render import render_ns
 @render_ns.route('/<string:user_id>')
 class Profile(Resource):
     @render_ns.doc(description='`user_id`를 가지는 얼굴이 제대로 드러나는 랜덤 포즈를 가지는 프로필 이미지를 반환합니다.')
+    @render_ns.produces(['image/png'])
     def get(self, user_id):
-        return {
-            'render': True
-        }
+        return send_file('../test.png')
