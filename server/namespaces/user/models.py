7ae3a21c7d36c8996dd9280f2eb5fc3c7c6f67be
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-11 19:12:47 +0900

    fix: add 제페토 ID, 국적 register fields and validator

--- server/namespaces/user/models.py
+++ server/namespaces/user/models.py
@@ -3,13 +3,17 @@ from server.namespaces.user import user_ns
 
 user_model = user_ns.model('UserModel', {
     '_id': fields.String(),
-    'email': fields.String(),
+    # 'email': fields.String(),
     'name': fields.String(),
-    'password': fields.String()
+    'password': fields.String(),
+    'zepeto_id': fields.String(),
+    'location': fields.String()
 })
 
 user_form_model = user_ns.model('UserFormModel', {
-    'email': fields.String(required=True, min_length=1),
+    # 'email': fields.String(required=True, min_length=1),
     'name': fields.String(required=True, min_length=1),
-    'password': fields.String(required=True, min_length=1)
+    'password': fields.String(required=True, min_length=1),
+    'zepeto_id': fields.String(required=True, min_length=5),
+    'location': fields.String(required=True)
 })
--- server/namespaces/user/resources/register.py
+++ server/namespaces/user/resources/register.py
@@ -3,19 +3,23 @@ from server.namespaces.user import user_ns
 from server.namespaces.user.models import user_form_model
 
 from functools import wraps
-import validators
+# import validators
 from flask import jsonify, request
 from server import mongo
 from server.namespaces import hash_password
 
+def location_validator(location):
+    if location in ['korea', 'english']:
+        return True
+    return False
 
 @user_ns.route('/register')
 class Register(Resource):
     @user_ns.expect(user_form_model, validate=True)
     @user_ns.doc(
         responses={200: '성공', 400: '잘못된 요청', 500: 'Unacknowledged'},
-        description='새로운 사용자를 추가합니다.')
-    @user_ns.validate('email', validators.email)
+        description='*사용자 이름, 비밀번호, 제페토 ID, 국적*을 받아 새로운 사용자를 추가합니다.')
+    @user_ns.validate('location', location_validator)
     def post(self):
         user = user_ns.payload
         user['password'] = hash_password(user['password'])
