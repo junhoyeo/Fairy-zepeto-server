37b47a9af892a06396d42427166e1cb5c80dd380
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-11 20:41:02 +0900

    feat: implement update_users

--- server/socket/resources/random.py
+++ server/socket/resources/random.py
@@ -1,19 +1,40 @@
+import random
 from server.socket import socketio
 from flask_jwt_extended import decode_token
 
 CHATS = [] # 현재 서버에 존재하는 chat 오브젝트
 USERS = [] # 현재 서버에 존재하는 user 중 chat에 소속 x -> user _id로 구성
 
-chat_object = {
-    'users': [], # user _id
-    'qustions': [] # 나왔던 질문 id?
-} # copy this to use
+# chat_object = {
+#     'users': [], # user _id
+#     'qustions': [] # 나왔던 질문 id?
+# }
 
 def update_users():
+    global CHATS, USERS
     print(CHATS, USERS)
+    if len(USERS) >= 2: # 2명 이상이라 매칭 가능
+        pairs = []
+        # print(USERS)
+        for pair in range(len(USERS) // 2):
+            pairs.append([
+                USERS.pop(random.randrange(len(USERS))), 
+                USERS.pop(random.randrange(len(USERS)))
+            ])
+        # print(pairs)
+        # 최대한 랜덤으로 짝을 만들어 CHAT을 생성한다.
+
+        for pair in pairs:
+            # print(pair)
+            CHATS.append({
+                'users': [pair[0], pair[1]]
+            })
+            # 그 다음에 각각의 사람들에게 emit함
+        print(CHATS, USERS)
 
 @socketio.on('match')
 def match(token): # 랜덤 매칭?
+    global CHATS, USERS
     print(CHATS, USERS)
     # 먼저 data에는, 사용자의 인증 정보(토큰)이 있음 -> 토큰이 유효한지 확인하고, 해당 토큰에서 제페토 아이디 같은 정보를 extract 해야 함
     # 유효하지 않으면 에러 던짐
@@ -36,6 +57,7 @@ def match(token): # 랜덤 매칭?
     if user_id not in USERS:
         USERS.append(user_id)
 
+    USERS += ['1gfsgfsgfsfsg4', '2fsgfgsgsffsggfs']
     # users가 업데이트될 때마다 update_users 같은 걸 실행해주자
     update_users()
     # update_users는 users에서 최대한 많이 랜덤 짝을 만들어서 걔네한테 이벤트를 emit해준다.
